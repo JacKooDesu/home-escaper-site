75dbbc98f12fcc9f6c410b739bde890eb9a32ca7
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2024-01-20 16:22:57 +0800

    add back button

--- js/page-handle.js
+++ js/page-handle.js
@@ -1,4 +1,5 @@
 var currentPage;
+var backMainBtn;
 
 function initPageHandle() {
   Array.prototype.filter.call(
@@ -6,11 +7,18 @@ function initPageHandle() {
     (e) => (e.onclick = () => activatePage(e.innerText))
   );
 
+  backMainBtn = document.getElementById("back-main");
+  backMainBtn.classList.add("is-next");
+  backMainBtn.onclick = () => activatePage("main");
+
   currentPage = document.getElementById("main");
 }
 
 function activatePage(tag) {
   wheelActivate = tag != "main";
+
+  setActivate(backMainBtn, wheelActivate);
+
   let old = currentPage;
   currentPage = document.getElementById(tag);
   current = 0;
@@ -20,4 +28,9 @@ function activatePage(tag) {
 
   currentPage.classList.remove("is-next");
   currentPage.classList.add("is-current");
+}
+
+function setActivate(target, b) {
+  target.classList.remove(!b ? "is-current" : "is-next");
+  target.classList.add(b ? "is-current" : "is-next");
 }
